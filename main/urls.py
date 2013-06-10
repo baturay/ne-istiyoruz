8ae7bd4b41e7168727968d8e4bfaddffac6ea0de
baturay <baturay.akaslan@tufts.edu>
2013-06-10 04:22:56 +0300

    Belki simdi?

--- main/urls.py
+++ main/urls.py
@@ -31,5 +31,5 @@ urlpatterns = patterns("",
     url("^users/(?P<username>.*)/comments/$",
         CommentList.as_view(), {"by_score": False},
         name="comment_list_user"),
-#    url("^accounts/profile/$", RedirectView.as_view(url='/account/update')),
+    url("^accounts/profile/$", RedirectView.as_view(url='')),
 )
